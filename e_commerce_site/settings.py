e7f462c043eecb7a02ff9d579d88ea5c3281e293
stuartj073 <stuartj073@gmail.com>
2021-12-03 11:54:18 +0000

    Added products app, models

--- e_commerce_site/settings.py
+++ e_commerce_site/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.account', # logging in/out registration
     'allauth.socialaccount', # loggin in using social media/google
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -139,6 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

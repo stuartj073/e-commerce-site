efe8d3950ac8b8ec12801d3b8dd68368d72caf10
stuartj073 <stuartj073@gmail.com>
2021-12-03 16:02:34 +0000

    Add search functionality

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
+
+""" Paths to each page from the home app """
 
 urlpatterns = [
     path('', views.index, name='home')

48c50e9964353c738c3e13161b70dce1a48e6664
stuartj073 <stuartj073@gmail.com>
2021-12-17 16:04:37 +0000

    Add django-countries to requirements.txt file

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product, Category
 from django.db.models.functions import Lower
+
+from .models import Product, Category
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -67,3 +69,43 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def add_product(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added product')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Faled to add product')
+    else:
+        form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form':form
+    }
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    form = ProductForm(instance=product)
+    messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
